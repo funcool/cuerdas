23afc7ba383ce5da93538653d99da88912845762
Andrey Antukh <niwi@niwi.be>
2014-12-08 17:23:53 +0100

    Add a bunch of functions to string core ns.

--- spec/cljs/string/core_spec.cljs
+++ spec/cljs/string/core_spec.cljs
@@ -1,11 +1,33 @@
 (ns string.core-spec
-  (:require-macros [speclj.core :refer [describe it should should-not run-specs]])
+  (:require-macros [speclj.core :refer [describe it should should= should-not run-specs]])
   (:require [speclj.core]
             [string.core :as str]))
 
 (describe "String basic operations"
-  (it "lower works as expected"
+  (it "lower"
     (should (= (str/lower "FOO") "foo")))
-  (it "upper works as expected"
+
+  (it "upper"
     (should (= (str/upper "foo") "FOO")))
+
+  (it "capitalize"
+    (should (= (str/capitalize "foo") "Foo")))
+
+  (it "collapse-whitespace"
+    (should= (str/collapse-whitespace "a  b\n c") "a b c"))
+
+  (it "contains?"
+    (should (str/contains? "abc" "ab")))
+
+  (it "startswith?"
+    (should (str/startswith? "abc" "ab"))
+    (should-not (str/startswith? "abc" "cab")))
+
+  (it "endswith?"
+    (should (str/endswith? "abc" "bc"))
+    (should-not (str/endswith? "abc" "bca")))
+
+  (it "trim"
+    (should= (str/trim " a ") "a")
+    (should= (str/trim "-a-" "-") "a"))
 )
--- src/cljs/string/core.cljs
+++ src/cljs/string/core.cljs
@@ -1,5 +1,6 @@
 (ns string.core
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [goog.string :as gstr]))
 
 (defn lower
   "Converts string to all lower-case."
@@ -10,3 +11,59 @@
   "Converts string to all upper-case."
   [s]
   (str/upper-case s))
+
+(defn capitalize
+  "Converts first letter of the string to uppercase."
+  [s]
+  (str/capitalize s))
+
+(defn collapse-whitespace
+  "Converts all adjacent whitespace characters
+  to a single space."
+  [s]
+  (gstr/collapseWhitespace s))
+
+(defn contains?
+  "Determines whether a string contains a substring."
+  [s subs]
+  (gstr/contains s subs))
+
+(defn startswith?
+  "Check if the string starts with prefix."
+  [s prefix]
+  (gstr/startsWith s prefix))
+
+(defn endswith?
+  "Check if the string ends with suffix."
+  [s prefix]
+  (gstr/endsWith s prefix))
+
+(defn camel-case
+  "Converts a string from selector-case to camelCase."
+  [s]
+  (gstr/toCamelCase s))
+
+(defn selector-case
+  "Converts a string from camelCase to selector-case."
+  [s]
+  (gstr/toSelectorCase s))
+
+(defn title-case
+  "Converts a string into TitleCase."
+  ([s]
+   (gstr/toTitleCase s))
+  ([s delimiters]
+   (gstr/toTitleCase s delimiters)))
+
+(defn escape-regexp
+  "Escapes characters in the string that are not safe
+  to use in a RegExp."
+  [s]
+  (gstr/regExpEscape s))
+
+(defn trim
+  ([s] (trim s " "))
+  ([s chs]
+   (let [rxstr (str "[" (escape-regexp chs) "]")
+         rx    (js/RegExp. (str "^" rxstr "+|" rxstr "+$") "g")]
+     (.replace s rx ""))))
