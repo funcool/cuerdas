e1738272766cc2886169dc46c537e4af2343895e
Andrey Antukh <niwi@niwi.be>
2014-12-23 18:00:16 +0100

    Set version to 0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cuerdas "0.1.0-SNAPSHOT"
+(defproject cuerdas "0.2.0-SNAPSHOT"
   :description "The missing string manipulation library for clojure"
   :url "https://github.com/funcool/cuerdas"
 
