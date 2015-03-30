a3bfdd20c80a0fa06bf5a32a1fccacea641bed27
Andrey Antukh <niwi@niwi.be>
2015-03-30 19:07:42 +0200

    Reorganize and update project.clj

--- project.clj
+++ project.clj
@@ -7,26 +7,34 @@
 
   :dependencies []
   :source-paths ["src/clj" "src/cljs"]
-  :cljsbuild {:test-commands {"test" ["phantomjs"  "bin/speclj" "target/tests.js"]}
-              :builds [{:id "dev"
-                        :source-paths ["target/spec/cljs" "src/cljs"]
-                        :notify-command ["phantomjs" "bin/speclj" "target/tests.js"]
-                        :compiler {:output-to "target/tests.js"
-                                   :optimizations :whitespace
-                                   :pretty-print true}}]}
+  :jar-exclusions [#"\.cljx|\.swp|\.swo|user.clj"]
 
-  :cljx {:builds [{:source-paths ["spec"]
-                   :output-path "target/spec/clj"
+  :cljx {:builds [{:source-paths ["test"]
+                   :output-path "output/test/clj"
                    :rules :clj}
-                  {:source-paths ["spec"]
-                   :output-path "target/spec/cljs"
+                  {:source-paths ["test"]
+                   :output-path "output/test/cljs"
                    :rules :cljs}]}
 
+  :cljsbuild {:test-commands {"test" ["node" "output/tests.js"]}
+              :builds [{:id "dev"
+                        :source-paths ["output/test/cljs" "src/cljs"]
+                        :notify-command ["node" "output/tests.js"]
+                        :compiler {:output-to "output/tests.js"
+                                   :output-dir "output/"
+                                   :source-map true
+                                   :static-fns true
+                                   :cache-analysis false
+                                   :main cuerdas.core-tests
+                                   :optimizations :none
+                                   :target :nodejs
+                                   :pretty-print true}}]}
+
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
-                                  [org.clojure/clojurescript "0.0-3058"]
-                                  [speclj "3.1.0"]]
-                   :test-paths ["target/spec/clj"]
-                   :plugins [[speclj "3.1.0"]
-                             [com.keminglabs/cljx "0.5.0" :exclusions [org.clojure/clojure]]
-                             [lein-cljsbuild "1.0.3"]]}})
+                                  [org.clojure/clojurescript "0.0-3126"]
+                                  [funcool/cljs-testrunners "0.1.0-SNAPSHOT"]]
+                   :test-paths ["output/test/clj"]
+                   :plugins [[com.keminglabs/cljx "0.6.0"
+                              :exclusions [org.clojure/clojure]]
+                             [lein-cljsbuild "1.0.4"]]}})
 
