39fde04fab3b60a72682909679c31f3fc546711c
Andrey Antukh <niwi@niwi.nz>
2015-12-03 22:51:17 +0200

    Update cljs compiler.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://opensource.org/licenses/BSD-2-Clause"}
 
   :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.7.170" :scope "provided"]]
+                 [org.clojure/clojurescript "1.7.189" :scope "provided"]]
   :source-paths ["src"]
   :jar-exclusions [#"\.swp|\.swo|user.clj"])
 
