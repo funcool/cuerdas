045a129076b28e5d733477b3b7042b7d8c87ccaa
Andrey Antukh <niwi@niwi.nz>
2016-04-12 17:54:54 +0300

    Update dependencies.

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "BSD (2-Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
 
-  :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.7.189" :scope "provided"]]
+  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
+                 [org.clojure/clojurescript "1.8.40" :scope "provided"]]
   :source-paths ["src"]
   :jar-exclusions [#"\.swp|\.swo|user.clj"])
 
