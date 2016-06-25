cce2a84584a0752152ca0fe3be8e99c36275df87
Andrey Antukh <niwi@niwi.nz>
2016-06-25 14:00:34 +0300

    Set version to 0.8.0

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject funcool/cuerdas "0.7.2"
+(defproject funcool/cuerdas "0.8.0"
   :description "The missing string manipulation library for clojure"
   :url "https://github.com/funcool/cuerdas"
   :license {:name "BSD (2-Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
 
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.8.40" :scope "provided"]]
+                 [org.clojure/clojurescript "1.9.89" :scope "provided"]]
   :source-paths ["src"]
   :jar-exclusions [#"\.swp|\.swo|user.clj"])
 
