ca4078e408e8c06089362b6043568f307d056020
Andrey Antukh <niwi@niwi.nz>
2016-08-09 22:36:06 +0300

    Add minimal externs for fix some interop not covered by gcc.

---
+++ src/cuerdas/core.externs.js
@@ -0,0 +1,16 @@
+/**
+ * @constructor
+ */
+var RegExp = function() {};
+
+/**
+ * @const
+ * @type {string}
+ */
+RegExp.prototype.flags;
+
+/**
+ * @const
+ * @type {string}
+ */
+RegExp.prototype.source;
