ae89a2719a57c05f039d91bf2a88e2e446990f36
Andrey Antukh <niwi@niwi.nz>
2016-08-29 21:17:27 +0300

    Import xregexp js library.

---
+++ assets/deps.cljs
@@ -0,0 +1,5 @@
+{:externs ["externs.js"]
+ :foreign-libs
+ [{:file "xregexp/xregexp.js"
+   :file-min "xregexp/xregexp.min.js"
+   :provides ["cuerdas.vendor.xregexp"]}]}
---
+++ assets/externs.js
@@ -0,0 +1,23 @@
+/**
+ * @constructor
+ */
+var RegExp = function() {};
+
+/**
+ * @const
+ * @type {?string}
+ */
+RegExp.prototype.flags;
+
+/**
+ * @const
+ * @type {string}
+ */
+RegExp.prototype.source;
+
+/**
+ * @param {string}  source
+ * @param {?string} flags
+ * @return {RegExp}
+ */
+var XRegExp = function(source, flags) {};
