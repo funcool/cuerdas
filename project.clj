6c7c505fa718d454599b4ca2fde4d413d788525f
Andrey Antukh <niwi@niwi.nz>
2016-08-29 21:18:32 +0300

    Update cljs depenency and add assets into source paths (project.clj).

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
             :url "http://opensource.org/licenses/BSD-2-Clause"}
 
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.89" :scope "provided"]]
-  :source-paths ["src"]
+                 [org.clojure/clojurescript "1.9.227" :scope "provided"]]
+  :source-paths ["src" "assets"]
+  :test-paths ["test"]
+
   :jar-exclusions [#"\.swp|\.swo|user.clj"])
 
