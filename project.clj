d96a14b80216bbc0bf05c0f66d999433d5ecd35d
Andrey Antukh <niwi@niwi.nz>
2016-10-23 15:35:47 +0200

    Set version to 2.0.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funcool/cuerdas "2.0.0-SNAPSHOT"
+(defproject funcool/cuerdas "2.0.0"
   :description "The missing string manipulation library for clojure"
   :url "https://github.com/funcool/cuerdas"
   :license {:name "BSD (2-Clause)"
