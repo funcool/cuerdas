f9904590044db9ce6d2495369cc131993d0f40bd
Andrey Antukh <niwi@niwi.nz>
2018-01-08 10:43:50 +0100

    Set clojure 1.9.0 as default clojure version.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "BSD (2-Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
 
-  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
                  [org.clojure/clojurescript "1.9.946" :scope "provided"]]
   :source-paths ["src" "assets"]
   :test-paths ["test"]
