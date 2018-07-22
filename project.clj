eb454c1cccea9337ad17beab9edfbc9ab6c1d57c
Andrey Antukh <niwi@niwi.nz>
2018-07-22 17:18:51 +0200

    Update dependencies.

--- project.clj
+++ project.clj
@@ -5,14 +5,16 @@
             :url "http://opensource.org/licenses/BSD-2-Clause"}
 
   :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.946" :scope "provided"]]
+                 [org.clojure/clojurescript "1.10.339" :scope "provided"]]
   :source-paths ["src" "assets"]
   :test-paths ["test"]
 
   :jar-exclusions [#"\.swp|\.swo|user.clj"]
 
   :profiles
-  {:dev {:aliases {"test-all" ["with-profile" "dev,1.8:dev,1.7:dev" "test"]}}
+  {:dev {:aliases {"test-all" ["with-profile" "dev,1.8:dev,1.7:dev" "test"]}
+         :plugins [[lein-ancient "0.6.15"]]}
+
    :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
    :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}})
 
